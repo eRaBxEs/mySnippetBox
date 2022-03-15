2925f8b0723612b755e27ced99e6a7872b596379
eRaBxEs <ehiojee@yahoo.com>
2022-03-15 07:31:37 +0100

    add a few patterns/paths

--- main.go
+++ main.go
@@ -11,17 +11,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
-func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Show a specific snippet ..."))
+}
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet ..."))
+}
+
+func main() {
+	// Register the two new handler functions and corresponding URL patterns with
+	// the servemux, in exactly the same way that we did before.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)                        // sub-tree path: ends with a trailing '/'
+	mux.HandleFunc("/snippet", showSnippet)          // fixed path: does not ends with a trailing '/'
+	mux.HandleFunc("/snippet/create", createSnippet) // fixed path: does not ends with a trailing '/'
+
 	log.Println("Started server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
