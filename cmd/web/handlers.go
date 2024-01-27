3c20a381071ba3d02bb738612c19070ff276df6f
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-01-27 19:21:38 +0000

    create a template set to add the html template file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -11,6 +13,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+
+	// use the template.ParseFiles() function to read the template file into a template set
+	// send a generic 500 internal server error incase of an error
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// use Execute() method on the template set to write the template content as a response body which for now we will leave as nil
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
 	w.Write([]byte("Hello Snippet!"))
 }
 
