621b4d54e20987928c2457c375ee573b3c8c6cf0
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-01-29 12:20:19 +0000

    use fileserver function to add static files to a url path

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,7 +6,17 @@ import (
 )
 
 func main() {
+
 	mux := http.NewServeMux()
+
+	// create a file server which serves files out of the "./ui/static/" directory
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// due to the behaviour of th http.FileServer() which removes the leading slash from the given URL path
+	// this will lead to searching for ./ui/static which is a path that does not exist
+	// so, we use http.StripPrefix() to then strip the leading "/static" before registering it to mux
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// register the other application routes as normal
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
